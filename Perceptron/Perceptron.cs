7bca05110d6c12e0d989a0c8f86780a62969959b
Nicholas Finefter <nfinefter@gmail.com>
2024-03-07 12:10:23 -0800

    Add project files.

---
+++ Perceptron/Perceptron.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perceptron
+{
+    public class Perceptron
+    {
+        double[] weights;
+        double bias;
+        public Perceptron(double[] initialWeightValues, double initialBiasValue)
+        { /*initializes the weights array and bias*/
+
+            weights = initialWeightValues;
+            bias = initialBiasValue;
+        }
+
+        public Perceptron(int amountOfInputs)
+        { /*Initializes the weights array given the amount of inputs*/
+
+            weights = new double[amountOfInputs];
+            
+        }
+
+        public void Randomize(Random random, double min, double max)
+        { /*Randomly generates values for every weight including the bias*/
+
+            for (int i = 0; i < weights.Length; i++)
+            {
+                weights[i] = random.Next((int)min, (int)max);
+            }
+
+        }
+
+        public double Compute(double[] inputs)
+        { /*computes the output with given input*/
+
+            double output = 0;
+
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                output += inputs[i] * weights[i];
+            }
+
+
+            return output + bias;
+        }
+
+        //public double[] Compute(double[][] inputs)
+        //{ /*computes the output for each row of inputs*/
+
+        //    double[] output = new double[inputs.Length / 2];
+
+        //    for (int i = 0; i < inputs.Length; i++)
+        //    {
+        //        for (int k = 0; k < inputs.Length; k++)
+        //        {
+        //            output += inputs[i][k] * weights[i];
+        //        }
+                
+        //    }
+
+
+        //    return output + bias;
+
+        //}
+    }
+}
---
+++ Perceptron/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Perceptron
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
