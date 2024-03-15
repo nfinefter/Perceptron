ce4bf5f49714cbcd3bb62f74fb4c55c7fdec93fb
nicholas.finefter <nfinefter@gmail.com>
2024-03-15 16:00:14 -0700

    Finished initial perceptron assignment

--- Perceptron/Perceptron.cs
+++ Perceptron/Perceptron.cs
@@ -10,20 +10,27 @@ namespace Perceptron
     {
         double[] weights;
         double bias;
-        public Perceptron(double[] initialWeightValues, double initialBiasValue)
+        double mutationAmount;
+        Random random;
+        Func<double, double, double> errorFunc;
+
+        public Perceptron(double[] initialWeightValues, double initialBiasValue, double mutationAmount, Random random, Func<double, double, double> errorFunc)
         { /*initializes the weights array and bias*/
 
             weights = initialWeightValues;
             bias = initialBiasValue;
+            this.mutationAmount = mutationAmount;
+            this.random = random;
+            this.errorFunc = errorFunc;
         }
-
-        public Perceptron(int amountOfInputs)
+        public Perceptron(int amountOfInputs, double mutationAmount, Random random, Func<double, double, double> errorFunc)
         { /*Initializes the weights array given the amount of inputs*/
 
             weights = new double[amountOfInputs];
-            
+            this.mutationAmount = mutationAmount;
+            this.random = random;
+            this.errorFunc = errorFunc;
         }
-
         public void Randomize(Random random, double min, double max)
         { /*Randomly generates values for every weight including the bias*/
 
@@ -33,7 +40,6 @@ namespace Perceptron
             }
 
         }
-
         public double Compute(double[] inputs)
         { /*computes the output with given input*/
 
@@ -47,24 +53,36 @@ namespace Perceptron
 
             return output + bias;
         }
+        public double[] Compute(double[][] inputs)
+        { /*computes the output for each row of inputs*/
 
-        //public double[] Compute(double[][] inputs)
-        //{ /*computes the output for each row of inputs*/
+            double[] output = new double[inputs.Length];
 
-        //    double[] output = new double[inputs.Length / 2];
-
-        //    for (int i = 0; i < inputs.Length; i++)
-        //    {
-        //        for (int k = 0; k < inputs.Length; k++)
-        //        {
-        //            output += inputs[i][k] * weights[i];
-        //        }
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                for (int k = 0; k < inputs[i].Length; k++)
+                {
+                    output[i] += inputs[i][k] * weights[k];
+                }
                 
-        //    }
+                output[i] += bias;
+            }
+
+            return output;
+
+        }
+        public double GetError(double[][] inputs, double[] desiredOutputs)
+        { /*computes the output using the inputs returns the average error between each output row and each desired output row using errorFunc*/ 
+
+            
+
+        }
+
+        public double TrainWithHillClimbing(double[][] inputs, double[] desiredOutputs, double currentError)
+        { /*attempts one hill climbing training iteration and returns the new current error*/ 
+        
 
 
-        //    return output + bias;
-
-        //}
+        }
     }
 }
--- Perceptron/Program.cs
+++ Perceptron/Program.cs
@@ -4,7 +4,18 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double[] initialWeightValues = { 0.75, -1.25};
+            const double initialBiasValue = 0.5;
+            double[][] inputValues = { new[]{0.0, 0.0}, new[]{0.3, -0.7}, new[]{ 1.0, 1.0 }, new[]{ -1.0, -1.0}, new[]{ -0.5, 0.5 } };
+
+            Perceptron perceptron = new Perceptron(initialWeightValues, initialBiasValue);
+
+            double[] output = perceptron.Compute(inputValues);
+
+            for (int i = 0; i < output.Length; i++)
+            {
+                Console.WriteLine($"{output[i]} \n");
+            }
         }
     }
 }
